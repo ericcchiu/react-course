1e84ecda51d4a41849b8e4cf8a7954434d3c1f9d
ericcchiu <eric.chn.chiu@gmail.com>
2018-10-29 10:37:18 -0500

    (feat) Add a person component with properties.

--- lectures/src/App.jsx
+++ lectures/src/App.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './components/Person.jsx'; 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>React Course</h1>
+        <Person name='James' age='25' />
+        <Person name='Kim' age='26' />
+        <Person name='Grayson' age='27' />
       </div>
     );
   }
