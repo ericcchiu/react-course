e546de77488ecee9c088d713a8445c58d9d81b90
ericcchiu <eric.chn.chiu@gmail.com>
2018-10-29 11:37:02 -0500

    (feat) Add switchNameHandler and changedNameHandler to app and person component.

--- lectures/src/App.jsx
+++ lectures/src/App.jsx
@@ -3,13 +3,67 @@ import './App.css';
 import Person from './components/Person.jsx'; 
 
 class App extends Component {
+  
+  // constructor(props) { 
+  //   super(props);
+  //   this.state = { 
+  //     persons: [ 
+  //       { name: 'James', age: 25 }, 
+  //       { name: 'Kim', age: 26 }, 
+  //       { name: 'Grayson', age: 27 }
+  //     ]
+  //   } 
+  // }
+
+  state = { 
+    persons: [ 
+            { name: 'James', age: 25 }, 
+            { name: 'Kim', age: 26 }, 
+            { name: 'Grayson', age: 27 }
+    ],
+    otherState: 'Some other state'
+  }
+  
+  switchNameHandler = (newName) => { 
+    console.log('Switch name button was clicked');
+    this.setState({
+      persons: [
+        { name: newName, age: 25 }, 
+        { name: 'Kim', age: 26 }, 
+        { name: 'Grayson', age: 27 }
+      ]
+    });
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({ 
+      persons: [
+        { name: 'Max', age: 25 }, 
+        { name: event.target.value, age: 26 }, 
+        { name: 'Jack', age: 27 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React Course</h1>
-        <Person name='James' age='25' />
-        <Person name='Kim' age='26' />
-        <Person name='Grayson' age='27' />
+        <button 
+          onClick={() => this.switchNameHandler('Max')}>Switch name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this,'Max!!!')} 
+        />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age} 
+          changed={this.nameChangedHandler}
+        />
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     );
   }
